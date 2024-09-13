682bbdd39f59a81894c515353e96623656d87527
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2024-09-12 22:57:05 -0400

    Initial Commit

---
+++ FlavorShare_iOS/Core/Recipe/Views/RecipeEditorView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeCreationView.swift
+//  FlavorShare_iOS
+//
+//  Created by Benjamin Lefebvre on 2024-09-12.
+//
+
+import SwiftUI
+
+struct RecipeEditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeEditorView()
+}
---
+++ FlavorShare_iOS/Core/Recipe/Views/RecipeView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeView.swift
+//  FlavorShare_iOS
+//
+//  Created by Benjamin Lefebvre on 2024-09-12.
+//
+
+import SwiftUI
+
+struct RecipeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeView()
+}
