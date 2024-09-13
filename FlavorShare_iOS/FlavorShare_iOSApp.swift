a7732167ca41b40be33af96ed0a771b48b5fd949
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2024-09-12 22:46:46 -0400

    Initial Commit

---
+++ FlavorShare_iOS/FlavorShare_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlavorShare_iOSApp.swift
+//  FlavorShare_iOS
+//
+//  Created by Benjamin Lefebvre on 2024-09-12.
+//
+
+import SwiftUI
+
+@main
+struct FlavorShare_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
