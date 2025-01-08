a745d74901a55c5fa98373e3b01b8ab614568005
Benjamin Lefebvre <benjamin.lefebvre.1024@gmail.com>
2025-01-08 14:46:37 -0500

    - Fix the measurement to have plural and singular versions - Reset Grocery List - Add new measurement units - Fix ingredient picker once click on it - New time/serving stepper allowing user to type number - Improved keyboard dismissal - Improved UI for the popup to add recipe to meal plan

---
+++ FlavorShare_iOS/Ressources/Unit.swift
@@ -0,0 +1,10 @@
+//
+//  Units.swift
+//  FlavorShare_iOS
+//
+//  Created by Benjamin Lefebvre on 2025-01-07.
+//
+
+class Unit {
+    static var measurements: [String] = ["", "bag", "bottle", "box", "bowl", "bunch", "bunch", "bun", "can", "carton", "clove", "container", "cube", "cup", "dash", "fillet", "glass", "g", "head", "handful", "jar", "kg", "l", "lb", "loaf", "ml", "oz", "pack", "packet", "piece", "pinch", "pint", "portion", "roll", "sheet", "shot", "splash", "sprig", "stick", "tbsp", "tsp", "tube", "unit", "wheel", "whole"]
+}
